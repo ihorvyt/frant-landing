df40acc65f708b758daf2b3d44871dfb51148943
ihorvyt <ihorvyt@gmail.com>
2024-08-29 23:08:11 +0300

    test perfomance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
             <FrantSection/>
             <SiteVariations/>
             <TechnologyStack/>
-            {/*<Services/>*/}
+            <Services/>
             <InfinitySlider />
             <CheckSection/>
             <TimeInfoSection/>
