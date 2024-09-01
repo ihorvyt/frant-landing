bda08e9d8c8241abd14014cd579d1ee39afcc922
ihorvyt <ihorvyt@gmail.com>
2024-09-01 17:48:01 +0300

    full website optimization test

--- src/components/Services/index.tsx
+++ src/components/Services/index.tsx
@@ -4,10 +4,9 @@ import './sevices.scss'
 import Design from "@/components/Services/Design";
 import Development from "@/components/Services/Development";
 import BrandIdentity from "@/components/Services/BrandingIdentity";
-import React, {useEffect, useRef, useState} from "react";
+import React, {useEffect, useState} from "react";
 import Glob3D from "@/components/Glob/Glob3D";
 import {useIntersectionObserver} from "@/hooks/useIntersectionObserver";
-import {string} from "prop-types";
 
 
 const Index = () => {
