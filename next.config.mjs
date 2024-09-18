ec0492e8b8c55686f74d62dbf882a03bbe112c08
ihorvyt <ihorvyt@gmail.com>
2024-09-18 19:02:56 +0300

    translate build fix 0.4.10

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
 };
 
 export default withNextIntl(nextConfig);
