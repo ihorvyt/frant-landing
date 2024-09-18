190f6ae648ab4071ddea4e0dca6bc1c333acb44f
ihorvyt <ihorvyt@gmail.com>
2024-09-18 21:37:48 +0300

    translate build fix 0.4.13

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,19 @@
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const withNextIntl = createNextIntlPlugin();
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',        // Забезпечує статичний експорт
-    trailingSlash: true,     // Додає слеш в кінці URL для коректної роботи на GitHub Pages
-    images: {
-        unoptimized: true      // Забороняє оптимізацію зображень
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/redirectRoute',
+                permanent: true,
+            },
+        ];
     },
 };
 
+const withNextIntl = createNextIntlPlugin();
+
+/** @type {import('next').NextConfig} */
 export default withNextIntl(nextConfig);
